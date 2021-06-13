7d70bb50308778da819f627a7dc578a436512753
Radostin Dimitrov <dimitrov.radostin@abv.bg>
2021-06-13 18:35:30 +0300

    new source added; results info fixed

--- main.js
+++ main.js
@@ -2,29 +2,33 @@
 // - fix the structure so not everything is in the Promise -- kind of
 // - more sources: code, lyrics, custom (from user) ...
 // - ? use a button to start
-// show spaces as the special symbol
 // after timeout show whole typed text with errors
-// start after typing strat not just the s 
-//    may be show some count down and then start
+// start after typing strat
+// may be show some count down and then start
 // optimise to remeber parsed source when loading them so that on multiple chnages to 
+// add quit option
+// group sources by category poems; book extracts; lyrics ...
 // https://developers.google.com/youtube/v3/docs/search/list
 // https://developers.google.com/youtube/player_parameters
 const IGNORE_AS_ERRORS = ['Shift', 'Ctrl'];
 const WORD_SEPARETORS_REGEXP = /\s/g;
+const FANSY_SPACE_SYMBOL = String.fromCharCode(9251);
+const FANSY_NEW_LINE_SYMBOL = String.fromCharCode(9166);
 const ESCAPE_ON_SCREEN = [
-    { "ESCAPE_REGEXP": /\n/g, "SHOW": String.fromCharCode(9166) },
-    { "ESCAPE_REGEXP": / /g, "SHOW": String.fromCharCode(9251) }
+    { "ESCAPE_REGEXP": /\n/g, "SHOW": FANSY_NEW_LINE_SYMBOL },
+    { "ESCAPE_REGEXP": / /g, "SHOW": FANSY_SPACE_SYMBOL }
 ];
 const SOURCE_LOCATION = 'https://dimitrov-radostin.github.io/typeTracker/';
+// const SOURCE_LOCATION = 'http://127.0.0.1:5500/'
 let loading_data = false;
 let typingData = {
     'string': '',
     'array_words': [],
-    'array_words_map': [],
+    'words_starting_positions': [],
     update: data => {
         typingData.string = data;
         typingData.array_words = data.split(WORD_SEPARETORS_REGEXP);
-        typingData.array_words_map = typingData.array_words
+        typingData.words_starting_positions = typingData.array_words
             .map(word => word.length + 1)
             .reduce((arr, currentWordLength) => [...arr, arr[arr.length - 1] + currentWordLength], [0]);
         ESCAPE_ON_SCREEN.forEach(v => {
@@ -32,8 +36,11 @@ let typingData = {
         });
         typingData.array_words = typingData.array_words
             .map((w, i) => i !== typingData.array_words.length ?
-            w + data[typingData.array_words_map[i + 1] - 1] :
-            w + String.fromCharCode(9166));
+            w + data[typingData.words_starting_positions[i + 1] - 1] :
+            w + FANSY_NEW_LINE_SYMBOL);
+    },
+    words_before_position: (position) => {
+        return typingData.words_starting_positions.findIndex(v => v > position) - 1;
     }
 };
 let position = 0;
@@ -54,10 +61,10 @@ document.getElementById("start_button")
     .addEventListener('click', startTracking);
 function startTracking() {
     if (loading_data) {
-        alert("losho");
+        alert("Tracking started before data is loaded.");
     }
     let TYPING_TIME = 1000 * document.getElementById("typing_time").value;
-    position = 0; // or leave as is to continue typing from where you left - have to add total time or to count typed symbols in yet another variable
+    position = 0;
     errors = [];
     typedWord = '';
     displayText(position);
@@ -68,10 +75,9 @@ function startTracking() {
     document.getElementById("timerWrapper").classList.toggle("hiddenTimer");
     writingField.children[0].textContent = '';
     writingField.children[1].textContent = '';
-    resultsDiv.children[0].textContent = '';
-    resultsDiv.children[1].textContent = '';
-    resultsDiv.children[2].textContent = '';
-    resultsDiv.children[3].textContent = '';
+    for (let child of resultsDiv.children) {
+        child.textContent = '';
+    }
     let updateTimerInterval = startTimer(TYPING_TIME);
     setTimeout(() => stopTracking(updateTimerInterval, keyDownHandler, TYPING_TIME), TYPING_TIME + 1);
 }
@@ -80,11 +86,12 @@ function stopTracking(updateTimerInterval, keyDownHandler, TYPING_TIME) {
     document.getElementById("darkLayer").style.display = "none";
     document.getElementById("timerWrapper").classList.toggle("hiddenTimer");
     // also display the whole text typed with errors made
-    // resultsDiv.children[0].textContent = `${results.wordsTyped} words`
-    resultsDiv.children[1].textContent = `${position} characters`;
-    resultsDiv.children[2].textContent = `${errors.length} errors`;
-    console.log('errors at positions ', errors);
-    // resultsDiv.children[3].textContent = `speed: ${results.wordsTyped / (TYPING_TIME / 60000)} words per minute`
+    let word_count = typingData.words_before_position(position);
+    resultsDiv.children[0].textContent = `characters: ${position}`;
+    resultsDiv.children[1].textContent = `words: ${word_count}`;
+    resultsDiv.children[2].textContent = `speed: ${word_count / (TYPING_TIME / 60000)} words/minute`;
+    resultsDiv.children[3].textContent = `errors: ${errors.length}`;
+    resultsDiv.children[4].textContent = `error rate: ${position ? (100 * errors.length / position) : 0}%`;
     clearInterval(updateTimerInterval);
 }
 function startTimer(TYPING_TIME) {
@@ -93,17 +100,16 @@ function startTimer(TYPING_TIME) {
     return updateTimerInterval;
 }
 function updateTimer(startTime, TYPING_TIME) {
-    // console.log()
     let timePassed = (Date.now() - startTime);
     timer.textContent = Math.round((TYPING_TIME - timePassed) / 1000).toString();
 }
 function displayText(position) {
-    let word_num = typingData.array_words_map.findIndex(v => v > position) - 1;
-    document.getElementById("currentWord").textContent = typingData.array_words[word_num];
-    document.getElementById("word2").textContent = typingData.array_words[word_num + 1];
-    document.getElementById("word3").textContent = typingData.array_words[word_num + 2];
-    document.getElementById("word4").textContent = typingData.array_words[word_num + 3];
-    document.getElementById("word5").textContent = typingData.array_words[word_num + 4];
+    let current_word_count = typingData.words_before_position(position);
+    document.getElementById("currentWord").textContent = typingData.array_words[current_word_count];
+    document.getElementById("word2").textContent = typingData.array_words[current_word_count + 1];
+    document.getElementById("word3").textContent = typingData.array_words[current_word_count + 2];
+    document.getElementById("word4").textContent = typingData.array_words[current_word_count + 3];
+    document.getElementById("word5").textContent = typingData.array_words[current_word_count + 4];
 }
 function loadAndDisplayText(source) {
     loading_data = true;
@@ -128,7 +134,7 @@ function parseData(rawData) {
             .map((a) => ({ sort: Math.random(), value: a }))
             .sort((a, b) => a.sort - b.sort)
             .map((a) => a.value)
-            .join('');
+            .join(''); // all words end with space in the json, so an empty str is usesd here
     }
     else if (rawData.type === 'string') {
         typingData = rawData.data;
@@ -140,7 +146,7 @@ function keyDownHandler(event) {
     // REMOVE EVENT LISTNER FOR START KEYWORD
     let letter = typingData.string[position];
     if (event.key === letter || (event.key === 'Enter' && letter.charCodeAt(0) === 10)) {
-        if (typingData.array_words_map.includes(position + 1)) {
+        if (typingData.words_starting_positions.includes(position + 1)) {
             typedWord = '';
             displayText(position + 1);
         }
--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@
 // - fix the structure so not everything is in the Promise -- kind of
 // - more sources: code, lyrics, custom (from user) ...
 // - ? use a button to start
-// show spaces as the special symbol
 // after timeout show whole typed text with errors
-// start after typing strat not just the s 
-//    may be show some count down and then start
-
+// start after typing strat
+// may be show some count down and then start
 // optimise to remeber parsed source when loading them so that on multiple chnages to 
+// add quit option
+// group sources by category poems; book extracts; lyrics ...
+
 
 // https://developers.google.com/youtube/v3/docs/search/list
 // https://developers.google.com/youtube/player_parameters
@@ -15,22 +16,25 @@
 
 const IGNORE_AS_ERRORS = ['Shift', 'Ctrl']
 const WORD_SEPARETORS_REGEXP = /\s/g
+const FANSY_SPACE_SYMBOL = String.fromCharCode(9251)
+const FANSY_NEW_LINE_SYMBOL = String.fromCharCode(9166)
 const ESCAPE_ON_SCREEN = [
-    { "ESCAPE_REGEXP": /\n/g, "SHOW": String.fromCharCode(9166) },
-    { "ESCAPE_REGEXP": / /g, "SHOW": String.fromCharCode(9251) }
+    { "ESCAPE_REGEXP": /\n/g, "SHOW": FANSY_NEW_LINE_SYMBOL },
+    { "ESCAPE_REGEXP": / /g, "SHOW": FANSY_SPACE_SYMBOL }
 ]
 const SOURCE_LOCATION = 'https://dimitrov-radostin.github.io/typeTracker/'
-
+// const SOURCE_LOCATION = 'http://127.0.0.1:5500/'
 let loading_data = false
+
 let typingData = {
     'string': '',
     'array_words': [],
-    'array_words_map': [],
+    'words_starting_positions': [],
 
     update: data => {
         typingData.string = data
         typingData.array_words = data.split(WORD_SEPARETORS_REGEXP)
-        typingData.array_words_map = typingData.array_words
+        typingData.words_starting_positions = typingData.array_words
             .map(word => word.length + 1)
             .reduce(
                 (arr, currentWordLength) =>
@@ -46,15 +50,18 @@ let typingData = {
             .map(
                 (w, i) =>
                     i !== typingData.array_words.length ?
-                        w + data[typingData.array_words_map[i + 1] - 1] :
-                        w + String.fromCharCode(9166)
+                        w + data[typingData.words_starting_positions[i + 1] - 1] :
+                        w + FANSY_NEW_LINE_SYMBOL
             )
+    },
+    
+    words_before_position: (position: number) => {
+        return typingData.words_starting_positions.findIndex(v => v > position) - 1
     }
 }
 
 let position = 0
 let errors = []
-
 
 const writingField = document.getElementById('writingField')
 const resultsDiv = document.getElementById('results')
@@ -76,11 +83,11 @@ document.getElementById("start_button")
 
 function startTracking() {
     if (loading_data) {
-        alert("losho")
+        alert("Tracking started before data is loaded.")
     }
     let TYPING_TIME = 1000 * document.getElementById("typing_time").value
 
-    position = 0  // or leave as is to continue typing from where you left - have to add total time or to count typed symbols in yet another variable
+    position = 0
 
     errors = []
     typedWord = ''
@@ -95,10 +102,10 @@ function startTracking() {
 
     writingField.children[0].textContent = ''
     writingField.children[1].textContent = ''
-    resultsDiv.children[0].textContent = '' 
-    resultsDiv.children[1].textContent = '' 
-    resultsDiv.children[2].textContent = ''
-    resultsDiv.children[3].textContent = ''
+    
+    for(let child of resultsDiv.children){
+        child.textContent = ''
+    }
     let updateTimerInterval = startTimer(TYPING_TIME)
     setTimeout(() => stopTracking(updateTimerInterval, keyDownHandler, TYPING_TIME), TYPING_TIME + 1)
 }
@@ -109,13 +116,12 @@ function stopTracking(updateTimerInterval, keyDownHandler, TYPING_TIME) {
     document.getElementById("timerWrapper").classList.toggle("hiddenTimer")
 
     // also display the whole text typed with errors made
-
-    // resultsDiv.children[0].textContent = `${results.wordsTyped} words`
-    resultsDiv.children[1].textContent = `${position} characters`
-    resultsDiv.children[2].textContent = `${errors.length} errors`
-    console.log('errors at positions ', errors)
-    // resultsDiv.children[3].textContent = `speed: ${results.wordsTyped / (TYPING_TIME / 60000)} words per minute`
-
+    let word_count = typingData.words_before_position(position)
+    resultsDiv.children[0].textContent = `characters: ${position}`
+    resultsDiv.children[1].textContent = `words: ${word_count}`
+    resultsDiv.children[2].textContent = `speed: ${word_count / (TYPING_TIME / 60000)} words/minute`
+    resultsDiv.children[3].textContent = `errors: ${errors.length}`
+    resultsDiv.children[4].textContent = `error rate: ${position? (100 * errors.length / position) : 0}%`
     clearInterval(updateTimerInterval)
 }
 
@@ -126,18 +132,17 @@ function startTimer(TYPING_TIME) {
 }
 
 function updateTimer(startTime, TYPING_TIME) {
-    // console.log()
     let timePassed = (Date.now() - startTime)
     timer.textContent = Math.round((TYPING_TIME - timePassed) / 1000).toString()
 }
 
 function displayText(position: number) {
-    let word_num = typingData.array_words_map.findIndex(v => v > position) - 1
-    document.getElementById("currentWord").textContent = typingData.array_words[word_num]
-    document.getElementById("word2").textContent = typingData.array_words[word_num + 1]
-    document.getElementById("word3").textContent = typingData.array_words[word_num + 2]
-    document.getElementById("word4").textContent = typingData.array_words[word_num + 3]
-    document.getElementById("word5").textContent = typingData.array_words[word_num + 4]
+    let current_word_count = typingData.words_before_position(position)
+    document.getElementById("currentWord").textContent = typingData.array_words[current_word_count]
+    document.getElementById("word2").textContent = typingData.array_words[current_word_count + 1]
+    document.getElementById("word3").textContent = typingData.array_words[current_word_count + 2]
+    document.getElementById("word4").textContent = typingData.array_words[current_word_count + 3]
+    document.getElementById("word5").textContent = typingData.array_words[current_word_count + 4]
 }
 
 function loadAndDisplayText(source) {
@@ -165,7 +170,7 @@ function parseData(rawData) {
             .map((a) => ({ sort: Math.random(), value: a }))
             .sort((a, b) => a.sort - b.sort)
             .map((a) => a.value)
-            .join('')
+            .join('')       // all words end with space in the json, so an empty str is usesd here
     } else if (rawData.type === 'string') {
         typingData = rawData.data
     }
@@ -178,7 +183,7 @@ function keyDownHandler(event) {
     let letter = typingData.string[position]
 
     if (event.key === letter || (event.key === 'Enter' && letter.charCodeAt(0) === 10)) {
-        if (typingData.array_words_map.includes(position + 1)) {
+        if (typingData.words_starting_positions.includes(position + 1)) {
             typedWord = ''
             displayText(position + 1)
         }
